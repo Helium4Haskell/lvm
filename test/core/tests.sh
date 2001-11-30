1da4c8a3561b05da09ee69e1f292371d6de5d54b
daan <unknown@mail>
2001-11-30 13:26:02 +0000

    make compiler and runtime before testing

--- test/core/tests.sh
+++ test/core/tests.sh
@@ -1,4 +1,27 @@
 date > test.log
+
+#make the compiler
+echo make the compiler >> test.log
+echo make the compiler
+cd ../../src/lib
+make > test.out 2> test.out
+cat test.out
+cat test.out >> test.log 
+cd ../../test/core
+echo >> test.log
+
+#make the system
+echo make the system >> test.log
+echo make the system
+cd ../../src/runtime
+make > test.out 2> test.out
+cat test.out
+cat test.out >> test.log 
+cd ../../test/core
+echo >> test.log
+cd ../../test/core
+
+#test all files
 for t in *.core; 
 {
   test.sh ${t%.core}
