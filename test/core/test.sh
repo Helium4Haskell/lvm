ff0cd6825e7cb042333e9929d997df5b57bff104
cvs-3 <unknown@mail>
2002-02-15 13:40:23 +0000

    *** empty log message ***

--- test/core/test.sh
+++ test/core/test.sh
@@ -7,16 +7,16 @@ rm -f $1.out
 echo running $1
 ../../src/runtime/lvmrun $1 >>$1.out   2>>$1.out
 
-if diff -b $1.out $1.ok >> /dev/null; then
+if diff -w $1.out $1.ok >> /dev/null; then
 echo ok
 else
 echo -- error --
 cat $1.out
-cmp $1.out $1.ok
+diff -w $1.out $1.ok
 echo -----------
 echo -- error -- >> test.log
 cat $1.out	 >> test.log
-cmp $1.out $1.ok >> test.log 2>> test.log;
+diff -w $1.out $1.ok >> test.log 2>> test.log;
 echo ----------- >> test.log
 fi
 
