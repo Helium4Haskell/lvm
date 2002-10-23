6fa7fc176682c360229df8a58bc85982a2edd2be
uust <unknown@mail>
2002-10-23 07:55:31 +0000

    Don't print "final value" anymore. Arjan

--- src/runtime/core/schedule.c
+++ src/runtime/core/schedule.c
@@ -89,9 +89,9 @@ extern wsize_t stack_wsize_max, stack_wsize_threshold, stack_wsize_init;
   case Thread_complete:
   case Thread_yield:
   default: {
-    print( "final value: " );
-    print_value(module, thread->stack_sp[0]);
-    print( "\n");
+//    print( "final value: " );
+//    print_value(module, thread->stack_sp[0]);
+//    print( "\n");
     break;
   }}
 
