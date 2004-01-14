1533ed3888c306763176c9061ba8d2a94fac81c2
cvs-3 <unknown@mail>
2004-01-14 14:46:59 +0000

    File permission on creation are set to read and write.

--- src/runtime/prim/primsys.c
+++ src/runtime/prim/primsys.c
@@ -186,9 +186,9 @@ long prim_open(const char* path, long flags, long perm)
 {
   int ret;
   ret = open(String_val(path), flags
-#if !macintosh
-             , Int_val(perm)
-#endif
+/* #if !macintosh */
+             , Int_val(perm) | _S_IREAD | _S_IWRITE
+/* #endif */
                                        );
   if (ret == -1) sys_error(copy_string(path));
   return ret;
