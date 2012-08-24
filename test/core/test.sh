3fec5a7582ed630b54dcf2318684e45a39b1be02
Bastiaan Heeren <Bastiaan.Heeren@ou.nl>
2012-08-24 08:45:40 +0000

    fixing test scripts

--- test/core/test.sh
+++ test/core/test.sh
@@ -1,24 +1,25 @@
 echo test $1 >> test.log
-echo compiling $1
+#echo compiling $1
 
 rm -f $1.out
 ../../src/lib/coreasm $1    >>$1.out  2>>$1.out
 
-echo running $1
-../../src/runtime/lvmrun -S200k -H4M $1 >>$1.out   2>>$1.out
+#echo running $1
+../../src/runtime/lvmrun -S200k -H4M -p $1 >>$1.out   2>>$1.out
 
 if compare $1.out $1.ok >> /dev/null; then
-echo ok
+echo $1 '(ok)'
 else
-echo -- error --
+echo
+echo -- ERROR: $1 --------------
 cat $1.out
 compare $1.out $1.ok
-echo -----------
-echo -- error -- >> test.log
+echo -----------------------------
+echo 
+echo -- ERROR: $1 -- >> test.log
 cat $1.out	 >> test.log
 compare $1.out $1.ok >> test.log 2>> test.log;
 echo ----------- >> test.log
 fi
 
 #echo >> test.log
-echo
