dcce952cfd56f35c272774c9920af1d63e6dcd42
Jurriaan Hage <J.Hage@uu.nl>
2012-10-04 11:58:12 +0000

    Resolved conflict between IntSet and Prelude due to use of foldr.

--- src/lib/Lvm/Common/IdSet.hs
+++ src/lib/Lvm/Common/IdSet.hs
@@ -18,7 +18,7 @@ module Lvm.Common.IdSet
    , listFromSet, setFromList, sizeSet, isEmptySet
    ) where
 
-import Data.IntSet
+import Data.IntSet (IntSet)
 import Data.List (sort)
 import Lvm.Common.Id
 import qualified Data.IntSet as IntSet
@@ -30,38 +30,38 @@ import qualified Data.IntSet as IntSet
 newtype IdSet = IdSet IntSet
 
 emptySet :: IdSet
-emptySet = IdSet empty
+emptySet = IdSet IntSet.empty
 
 singleSet :: Id -> IdSet
-singleSet = IdSet . singleton . intFromId
+singleSet = IdSet . IntSet.singleton . intFromId
 
 elemSet :: Id -> IdSet -> Bool
-elemSet x (IdSet s) = member (intFromId x) s
+elemSet x (IdSet s) = IntSet.member (intFromId x) s
 
 filterSet :: (Id -> Bool) -> IdSet -> IdSet
 filterSet p (IdSet s) = IdSet (IntSet.filter (p . idFromInt) s)
 
 foldSet :: (Id -> a ->  a) -> a -> IdSet -> a
-foldSet f a (IdSet s) = fold (f . idFromInt) a s
+foldSet f a (IdSet s) = IntSet.fold (f . idFromInt) a s
 
 insertSet :: Id -> IdSet -> IdSet
-insertSet x (IdSet s) = IdSet (insert (intFromId x) s)
+insertSet x (IdSet s) = IdSet (IntSet.insert (intFromId x) s)
 
 deleteSet :: Id -> IdSet -> IdSet
-deleteSet x (IdSet s) = IdSet (delete (intFromId x) s)
+deleteSet x (IdSet s) = IdSet (IntSet.delete (intFromId x) s)
 
 unionSet :: IdSet -> IdSet -> IdSet
-unionSet (IdSet s1) (IdSet s2) = IdSet (s1 `union` s2)
+unionSet (IdSet s1) (IdSet s2) = IdSet (s1 `IntSet.union` s2)
 
 unionSets :: [IdSet] -> IdSet
-unionSets xs = IdSet (unions [ s | IdSet s <- xs ])
+unionSets xs = IdSet (IntSet.unions [ s | IdSet s <- xs ])
 
 diffSet :: IdSet -> IdSet -> IdSet
-diffSet (IdSet s1) (IdSet s2) = IdSet (difference s1 s2)
+diffSet (IdSet s1) (IdSet s2) = IdSet (IntSet.difference s1 s2)
 
 -- sort is needed to not rely on an id's index
 listFromSet :: IdSet -> [Id]
-listFromSet (IdSet s) = sort [ idFromInt n | n <- elems s ]
+listFromSet (IdSet s) = sort [ idFromInt n | n <- IntSet.elems s ]
 
 setFromList :: [Id] -> IdSet
 setFromList = foldr insertSet emptySet
